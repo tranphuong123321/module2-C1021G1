e485ffcdb4a1eb5f28ddad53cf07d7e937b721f7
tranphuong123321 <phuong23628@gmail.com>
2021-12-20 08:24:23 +0700

    push baitap

---
+++ src/bai_13/bai_tap/tim_chuoi_tang_dan_co_do_dai_lon_nhat/Main.java
@@ -0,0 +1,46 @@
+package bai_13.bai_tap.tim_chuoi_tang_dan_co_do_dai_lon_nhat;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nhập chuỗi: ");
+        String string = input.nextLine();
+
+        LinkedList<Character> max = new LinkedList<>();
+
+
+        for (int i = 0; i < string.length(); i++) {
+
+            LinkedList<Character> list = new LinkedList<>();
+
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+
+                if (string.charAt(j) > list.getLast()) {
+
+                    list.add(string.charAt(j));
+                }
+            }
+
+            if (list.size() > max.size()) {
+
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+
+
+        for (Character ch: max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+
+
+}
